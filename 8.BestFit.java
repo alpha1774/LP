7aee743225211c23bca36550aa5cdb66ced697f3
alpha1774 <thisisharshal10@gmail.com>
2025-11-05 23:36:47 +0530

    Update 8.BestFit.java

--- 8.BestFit.java
+++ 8.BestFit.java
@@ -1,46 +1,68 @@
 import java.util.*;
 
-public class BestFit {
+class BestFit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // Input: Number of memory blocks
         System.out.print("Enter number of memory blocks: ");
-        int m = sc.nextInt();
-        int[] block = new int[m];
+        int nb = sc.nextInt();
+        int[] block = new int[nb];     // Initial block sizes
+        int[] rem = new int[nb];       // Remaining sizes after allocation
+
         System.out.println("Enter block sizes:");
-        for (int i = 0; i < m; i++) block[i] = sc.nextInt();
+        for (int i = 0; i < nb; i++) {
+            block[i] = sc.nextInt();
+            rem[i] = block[i]; // Initially, remaining = full block size
+        }
 
+        // Input: Number of processes
         System.out.print("Enter number of processes: ");
-        int n = sc.nextInt();
-        int[] process = new int[n];
+        int np = sc.nextInt();
+        int[] process = new int[np];   // Process sizes
+        int[] alloc = new int[np];     // Allocation index (-1 if not allocated)
+
         System.out.println("Enter process sizes:");
-        for (int i = 0; i < n; i++) process[i] = sc.nextInt();
+        for (int i = 0; i < np; i++) {
+            process[i] = sc.nextInt();
+            alloc[i] = -1; // Default: not allocated
+        }
 
-        int[] allocation = new int[n];
-        Arrays.fill(allocation, -1);
+        // Best Fit Allocation Logic
+        for (int i = 0; i < np; i++) {
+            int bestIndex = -1;
 
-        for (int i = 0; i < n; i++) {
-            int bestIdx = -1;
-            for (int j = 0; j < m; j++) {
-                if (block[j] >= process[i]) {
-                    if (bestIdx == -1 || block[j] < block[bestIdx])
-                        bestIdx = j;
+            for (int j = 0; j < nb; j++) {
+                if (rem[j] >= process[i]) {
+                    if (bestIndex == -1 || rem[j] < rem[bestIndex]) {
+                        bestIndex = j;
+                    }
                 }
             }
-            if (bestIdx != -1) {
-                allocation[i] = bestIdx;
-                block[bestIdx] -= process[i];
+
+            if (bestIndex != -1) {
+                alloc[i] = bestIndex;
+                rem[bestIndex] -= process[i];
             }
         }
 
-        System.out.println("\nBest Fit Allocation:");
-        System.out.println("Process\tSize\tBlock");
-        for (int i = 0; i < n; i++) {
-            System.out.print((i + 1) + "\t" + process[i] + "\t");
-            if (allocation[i] != -1)
-                System.out.println(allocation[i] + 1);
+        // Output: Allocation Table
+        System.out.println("\nProcess\tSize\tBlock Allocated");
+        for (int i = 0; i < np; i++) {
+            if (alloc[i] != -1)
+                System.out.println("P" + (i + 1) + "\t" + process[i] + "\tBlock " + (alloc[i] + 1));
             else
-                System.out.println("Not Allocated");
+                System.out.println("P" + (i + 1) + "\t" + process[i] + "\tNot Allocated");
         }
-        sc.close();
+
+        // Output: Fragmentation Report
+        System.out.println("\nBlock\tInitial Size\tRemaining\tInternal Fragmentation");
+        int totalFrag = 0;
+        for (int i = 0; i < nb; i++) {
+            System.out.println("B" + (i + 1) + "\t" + block[i] + "\t\t" + rem[i] + "\t\t" + rem[i]);
+            totalFrag += rem[i];
+        }
+
+        System.out.println("\nTotal Internal Fragmentation: " + totalFrag);
     }
 }
